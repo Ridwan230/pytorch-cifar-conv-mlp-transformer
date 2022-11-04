f3f8871e00bb8f5486d8c6e8d7dea34489aceb2b
Ridwan230 <191.ridwan@gmail.com>
2022-11-04 17:43:04 +0600

    MWE added

---
+++ models/layers.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+
+
+class Linear(nn.Linear):
+    def init(self, in_features, out_features, bias=True):
+        super(Linear, self).init(in_features, out_features, bias)
+        self.count = 0
+
+    def forward(self, x):
+        weight = self.weight
+        # MWE
+        max_val = (1.0 + 0.2) * \
+            torch.max(torch.max(weight), -1 * torch.min(weight))
+        if self.count:
+            weight = max_val * 0.5 * \
+                torch.log((1+weight/max_val) / (1-weight/max_val))
+        else:
+            self.count = 1
+        return F.Linear(x, weight, bias=True)
--- models/mlpmixer.py
+++ models/mlpmixer.py
@@ -2,8 +2,11 @@
 from torch import nn
 from functools import partial
 from einops.layers.torch import Rearrange, Reduce
+import layers as L
 
-pair = lambda x: x if isinstance(x, tuple) else (x, x)
+
+def pair(x): return x if isinstance(x, tuple) else (x, x)
+
 
 class PreNormResidual(nn.Module):
     def __init__(self, dim, fn):
@@ -14,7 +17,8 @@ class PreNormResidual(nn.Module):
     def forward(self, x):
         return self.fn(self.norm(x)) + x
 
-def FeedForward(dim, expansion_factor = 4, dropout = 0., dense = nn.Linear):
+
+def FeedForward(dim, expansion_factor=4, dropout=0., dense=L.Linear):
     inner_dim = int(dim * expansion_factor)
     return nn.Sequential(
         dense(dim, inner_dim),
@@ -24,20 +28,25 @@ def FeedForward(dim, expansion_factor = 4, dropout = 0., dense = nn.Linear):
         nn.Dropout(dropout)
     )
 
-def MLPMixer(*, image_size, channels, patch_size, dim, depth, num_classes, expansion_factor = 4, expansion_factor_token = 0.5, dropout = 0.):
+
+def MLPMixer(*, image_size, channels, patch_size, dim, depth, num_classes, expansion_factor=4, expansion_factor_token=0.5, dropout=0.):
     image_h, image_w = pair(image_size)
-    assert (image_h % patch_size) == 0 and (image_w % patch_size) == 0, 'image must be divisible by patch size'
+    assert (image_h % patch_size) == 0 and (image_w %
+                                            patch_size) == 0, 'image must be divisible by patch size'
     num_patches = (image_h // patch_size) * (image_w // patch_size)
-    chan_first, chan_last = partial(nn.Conv1d, kernel_size = 1), nn.Linear
+    chan_first, chan_last = partial(nn.Conv1d, kernel_size=1), L.Linear
 
     return nn.Sequential(
-        Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
-        nn.Linear((patch_size ** 2) * channels, dim),
+        Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',
+                  p1=patch_size, p2=patch_size),
+        L.Linear((patch_size ** 2) * channels, dim),
         *[nn.Sequential(
-            PreNormResidual(dim, FeedForward(num_patches, expansion_factor, dropout, chan_first)),
-            PreNormResidual(dim, FeedForward(dim, expansion_factor_token, dropout, chan_last))
+            PreNormResidual(dim, FeedForward(
+                num_patches, expansion_factor, dropout, chan_first)),
+            PreNormResidual(dim, FeedForward(
+                dim, expansion_factor_token, dropout, chan_last))
         ) for _ in range(depth)],
         nn.LayerNorm(dim),
         Reduce('b n c -> b c', 'mean'),
-        nn.Linear(dim, num_classes)
+        L.Linear(dim, num_classes)
     )
