c4722ed497899b9aca2a896d5c1cd87baa926139
Ridwan230 <191.ridwan@gmail.com>
2023-03-08 01:58:00 +0600

    Modified CBAM added in ResNet

--- models/resnet.py
+++ models/resnet.py
@@ -9,6 +9,7 @@ Reference:
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from . import layers as L
 
 
 class BasicBlock(nn.Module):
@@ -16,16 +17,19 @@ class BasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv1 = L.Conv2d(in_planes, planes, kernel_size=3,
+                              stride=stride, padding=1, bias=False)
+        self.bn1 = L.BatchNorm2d(planes)
+        self.conv2 = L.Conv2d(planes, planes, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn2 = L.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                L.Conv2d(in_planes, self.expansion*planes,
+                         kernel_size=1, stride=stride, bias=False),
+                L.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -41,18 +45,21 @@ class Bottleneck(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.conv1 = L.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = L.BatchNorm2d(planes)
+        self.conv2 = L.Conv2d(planes, planes, kernel_size=3,
+                              stride=stride, padding=1, bias=False)
+        self.bn2 = L.BatchNorm2d(planes)
+        self.conv3 = L.Conv2d(planes, self.expansion *
+                              planes, kernel_size=1, bias=False)
+        self.bn3 = L.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                L.Conv2d(in_planes, self.expansion*planes,
+                         kernel_size=1, stride=stride, bias=False),
+                L.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -69,8 +76,9 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = L.Conv2d(3, 64, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn1 = L.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -98,24 +106,28 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2,2,2,2])
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
 
 def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
 
 def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
 
 def ResNet101():
-    return ResNet(Bottleneck, [3,4,23,3])
+    return ResNet(Bottleneck, [3, 4, 23, 3])
+
 
 def ResNet152():
-    return ResNet(Bottleneck, [3,8,36,3])
+    return ResNet(Bottleneck, [3, 8, 36, 3])
 
 
 def test():
     net = ResNet18()
-    y = net(torch.randn(1,3,32,32))
+    y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
 
 # test()
