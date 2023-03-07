2ea7a2de74f4f5b5ff6482f040221b325184d8ca
Ridwan230 <191.ridwan@gmail.com>
2023-03-08 02:04:22 +0600

    BatchNorm Fixed

--- models/resnet.py
+++ models/resnet.py
@@ -19,17 +19,17 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = L.Conv2d(in_planes, planes, kernel_size=3,
                               stride=stride, padding=1, bias=False)
-        self.bn1 = L.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = L.Conv2d(planes, planes, kernel_size=3,
                               stride=1, padding=1, bias=False)
-        self.bn2 = L.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 L.Conv2d(in_planes, self.expansion*planes,
                          kernel_size=1, stride=stride, bias=False),
-                L.BatchNorm2d(self.expansion*planes)
+                nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -46,20 +46,20 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = L.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = L.BatchNorm2d(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = L.Conv2d(planes, planes, kernel_size=3,
                               stride=stride, padding=1, bias=False)
-        self.bn2 = L.BatchNorm2d(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = L.Conv2d(planes, self.expansion *
                               planes, kernel_size=1, bias=False)
-        self.bn3 = L.BatchNorm2d(self.expansion*planes)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 L.Conv2d(in_planes, self.expansion*planes,
                          kernel_size=1, stride=stride, bias=False),
-                L.BatchNorm2d(self.expansion*planes)
+                nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -78,7 +78,7 @@ class ResNet(nn.Module):
 
         self.conv1 = L.Conv2d(3, 64, kernel_size=3,
                               stride=1, padding=1, bias=False)
-        self.bn1 = L.BatchNorm2d(64)
+        self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
