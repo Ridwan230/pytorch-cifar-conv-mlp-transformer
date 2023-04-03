0bd23aeb261ecc86fad4a51ad7269c543a3ea86f
Ridwan230 <191.ridwan@gmail.com>
2023-04-04 03:33:58 +0600

    MWE replacing others

--- models/layers.py
+++ models/layers.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 from torch.nn.parameter import Parameter
 from torch.nn import functional as F
 
-# Modified Cbam
+
+# MWE IMPLEMENTATION
 
 
 class Conv2d(nn.Conv2d):
@@ -11,81 +12,106 @@ class Conv2d(nn.Conv2d):
                  padding=0, dilation=1, groups=1, bias=True):
         super(Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
                                      padding, dilation, groups, bias)
-
         self.count = 0
-        planes = in_channels
         self.eps = 1e-5
-        ratio = 16
-
-        self.mlp = nn.Sequential(
-            nn.Conv2d(planes//groups, max(planes//16, 1), kernel_size=1),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(max(planes//16, 1), planes//groups, kernel_size=1)
-        )
-        self.conv = nn.Conv2d(2, 1, kernel_size=1)
-
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.conv = nn.Conv2d(
-            2, 1, kernel_size=kernel_size, padding=kernel_size//2)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-
         weight = self.weight
-        # weight_avg = weight.mean(dim=1, keepdim=True).mean(
-        #     dim=2, keepdim=True).mean(dim=3, keepdim=True)
-        # weight = weight - weight_avg
-        # std = weight.view(weight.size(
-        #     0), -1).std(dim=1).view(-1, 1, 1, 1) + self.eps
-        # weight = weight / std.expand_as(weight)
-
-        wght1 = self.avg_pool(weight)
-        wght1 = self.mlp(wght1)
-        wght2 = self.max_pool(weight)
-        wght2 = self.mlp(wght2)
-        wght = torch.sigmoid(wght1+wght2)
-        skip_1 = weight * wght
-        weight = weight + skip_1
-
-        # spatial attention
-        max_result, _ = torch.max(weight, dim=1, keepdim=True)
-        avg_result = torch.mean(weight, dim=1, keepdim=True)
-        result = torch.cat([max_result, avg_result], 1)
-        wght_2 = self.conv(result)
-        wght_2 = self.sigmoid(wght_2)
-        skip_2 = weight*wght_2
-
-        weight = weight + skip_1 + skip_2
-
-        # Triple Attn
-
-        # weight2 = weight
-        # x = torch.cat((torch.max(weight2, 1)[0].unsqueeze(
-        #     1), torch.mean(weight2, 1).unsqueeze(1)), dim=1)
-        # x = self.conv(x)
-        # x = torch.sigmoid_(x)
-
-        # weight3 = weight
-        # x_perm1 = weight3.permute(0, 2, 1, 3).contiguous()
-        # x_perm1 = torch.cat((torch.max(x_perm1, 1)[0].unsqueeze(
-        #     1), torch.mean(x_perm1, 1).unsqueeze(1)), dim=1)
-        # x_perm1 = self.conv(x_perm1)
-        # x_perm1 = torch.sigmoid_(x_perm1)
-        # x_perm1 = x_perm1.permute(0, 2, 1, 3).contiguous()
-
-        # weight4 = weight
-        # x_perm2 = weight4.permute(0, 3, 2, 1).contiguous()
-        # x_perm2 = torch.cat((torch.max(x_perm2, 1)[0].unsqueeze(
-        #     1), torch.mean(x_perm2, 1).unsqueeze(1)), dim=1)
-        # x_perm2 = self.conv(x_perm2)
-        # x_perm2 = torch.sigmoid_(x_perm2)
-        # x_perm2 = x_perm2.permute(0, 3, 2, 1).contiguous()
-
-        # weight = weight * ((1/3)*(x+x_perm1+x_perm2))
-
+        # MWE
+        max_val = (1.0 + 0.1) * \
+            torch.max(torch.max(weight), -1 * torch.min(weight))
+        if self.count:
+            weight = max_val * 0.5 * \
+                torch.log((1+weight/max_val) / (1-weight/max_val))
+        else:
+            self.count = 1
         return F.conv2d(x, weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
+
+
+# Modified Cbam
+
+
+# class Conv2d(nn.Conv2d):
+#     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+#                  padding=0, dilation=1, groups=1, bias=True):
+#         super(Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
+#                                      padding, dilation, groups, bias)
+
+#         self.count = 0
+#         planes = in_channels
+#         self.eps = 1e-5
+#         ratio = 16
+
+#         self.mlp = nn.Sequential(
+#             nn.Conv2d(planes//groups, max(planes//16, 1), kernel_size=1),
+#             nn.LeakyReLU(inplace=True),
+#             nn.Conv2d(max(planes//16, 1), planes//groups, kernel_size=1)
+#         )
+#         self.conv = nn.Conv2d(2, 1, kernel_size=1)
+
+#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
+#         self.max_pool = nn.AdaptiveMaxPool2d(1)
+#         self.conv = nn.Conv2d(
+#             2, 1, kernel_size=kernel_size, padding=kernel_size//2)
+#         self.sigmoid = nn.Sigmoid()
+
+#     def forward(self, x):
+
+#         weight = self.weight
+#         # weight_avg = weight.mean(dim=1, keepdim=True).mean(
+#         #     dim=2, keepdim=True).mean(dim=3, keepdim=True)
+#         # weight = weight - weight_avg
+#         # std = weight.view(weight.size(
+#         #     0), -1).std(dim=1).view(-1, 1, 1, 1) + self.eps
+#         # weight = weight / std.expand_as(weight)
+
+#         wght1 = self.avg_pool(weight)
+#         wght1 = self.mlp(wght1)
+#         wght2 = self.max_pool(weight)
+#         wght2 = self.mlp(wght2)
+#         wght = torch.sigmoid(wght1+wght2)
+#         skip_1 = weight * wght
+#         weight = weight + skip_1
+
+#         # spatial attention
+#         max_result, _ = torch.max(weight, dim=1, keepdim=True)
+#         avg_result = torch.mean(weight, dim=1, keepdim=True)
+#         result = torch.cat([max_result, avg_result], 1)
+#         wght_2 = self.conv(result)
+#         wght_2 = self.sigmoid(wght_2)
+#         skip_2 = weight*wght_2
+
+#         weight = weight + skip_1 + skip_2
+
+#         # Triple Attn
+
+#         # weight2 = weight
+#         # x = torch.cat((torch.max(weight2, 1)[0].unsqueeze(
+#         #     1), torch.mean(weight2, 1).unsqueeze(1)), dim=1)
+#         # x = self.conv(x)
+#         # x = torch.sigmoid_(x)
+
+#         # weight3 = weight
+#         # x_perm1 = weight3.permute(0, 2, 1, 3).contiguous()
+#         # x_perm1 = torch.cat((torch.max(x_perm1, 1)[0].unsqueeze(
+#         #     1), torch.mean(x_perm1, 1).unsqueeze(1)), dim=1)
+#         # x_perm1 = self.conv(x_perm1)
+#         # x_perm1 = torch.sigmoid_(x_perm1)
+#         # x_perm1 = x_perm1.permute(0, 2, 1, 3).contiguous()
+
+#         # weight4 = weight
+#         # x_perm2 = weight4.permute(0, 3, 2, 1).contiguous()
+#         # x_perm2 = torch.cat((torch.max(x_perm2, 1)[0].unsqueeze(
+#         #     1), torch.mean(x_perm2, 1).unsqueeze(1)), dim=1)
+#         # x_perm2 = self.conv(x_perm2)
+#         # x_perm2 = torch.sigmoid_(x_perm2)
+#         # x_perm2 = x_perm2.permute(0, 3, 2, 1).contiguous()
+
+#         # weight = weight * ((1/3)*(x+x_perm1+x_perm2))
+
+#         return F.conv2d(x, weight, self.bias, self.stride,
+#                         self.padding, self.dilation, self.groups)
 
 # LWE
 
